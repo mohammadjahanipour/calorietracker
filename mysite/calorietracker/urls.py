e96e446b565156b8a4da56e1de2ea75e4645bdb7
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-09-25 19:32:13 -0400

    Initialize 4 basic views (profile, log, charts, analytics)

--- mysite/calorietracker/urls.py
+++ mysite/calorietracker/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view()),
-
-    path('register', views.Register.as_view(), name="register"),
-    path('login', views.Login.as_view(), name="login"),
-    path('logout', views.Logout.as_view(), name="logout"),
-
-
+    path("", views.HomePage.as_view()),
+    path("profile/", views.Profile.as_view(), name="profile"),
+    path("logdata/", views.LogData.as_view(), name="logdata"),
+    path("charts/", views.Charts.as_view(), name="charts"),
+    path("analytics/", views.Analytics.as_view(), name="analytics"),
+    path("register/", views.Register.as_view(), name="register"),
+    path("login/", views.Login.as_view(), name="login"),
+    path("logout/", views.Logout.as_view(), name="logout"),
 ]
--- mysite/calorietracker/views.py
+++ mysite/calorietracker/views.py
@@ -13,15 +13,32 @@ class HomePage(TemplateView):
     template_name = "calorietracker/home.html"
 
 
+class Profile(TemplateView):
+    template_name = "calorietracker/profile.html"
+
+
+class Charts(TemplateView):
+    template_name = "calorietracker/charts.html"
+
+
+class Analytics(TemplateView):
+    template_name = "calorietracker/analytics.html"
+
+
+class LogData(TemplateView):
+    # form_class = LogDataForm
+    template_name = "calorietracker/logdata.html"
+
+
 class Register(CreateView):
     form_class = RegisterForm
-    success_url = reverse_lazy('login')
-    template_name = 'calorietracker/register.html'
+    success_url = reverse_lazy("login")
+    template_name = "calorietracker/register.html"
 
 
 class Login(LoginView):
     form_class = LoginForm
-    template_name = 'calorietracker/login.html'
+    template_name = "calorietracker/login.html"
 
 
 class Logout(LogoutView):
