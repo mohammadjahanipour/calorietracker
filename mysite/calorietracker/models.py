15259aba6b6f04520e6460fe5192b83f7526b6c5
Balu <Balu+gitt@bluebeamproject.com>
2020-09-26 01:50:30 +0100

    Setting Model

--- mysite/calorietracker/models.py
+++ mysite/calorietracker/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from . base_models import DateTimeFields
+from django.contrib.auth import get_user_model
+from safedelete.models import SafeDeleteModel
 
-# Create your models here.
+
+class Setting(DateTimeFields, SafeDeleteModel):
+    """
+      - Age
+      - Sex/gender
+      - Height
+      - Perceived activity level
+      - Goal - maintain, lose, gain
+      - Goal date - by when?
+      - *Goal per week - caloric deficit/surprlus/goal for week, pounds per week ** not yet implemented
+    """
+
+    user = models.OneToOneField(
+        get_user_model(), unique=True, blank=False, null=False, on_delete=models.CASCADE)
+
+    age = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    # this should represent a range from 0 - 100 # TODO: add min max value validation
+    activity = models.IntegerField(blank=True, null=True)
+
+    goal_choices = [
+        ('L', 'Lose'),
+        ('M', 'Maintain'),
+        ('G', 'Gain'),
+        ]
+    goal = models.CharField(
+        max_length=2,
+        choices=goal_choices,
+        default='Maintain',
+    )
+    goal_date = models.DateTimeField(blank=True, null=True)
