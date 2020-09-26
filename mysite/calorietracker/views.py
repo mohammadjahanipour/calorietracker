12ac08e64a274a928a06b0c6a06ac480b8a5d60b
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-09-26 13:17:31 -0400

    Gate log data behind LoginMixIn

--- mysite/calorietracker/views.py
+++ mysite/calorietracker/views.py
@@ -4,9 +4,7 @@ from django.views.generic import TemplateView, CreateView, FormView, UpdateView
 from django.urls import reverse_lazy
 from .forms import RegisterForm, LoginForm, LogForm
 from .models import Log
-
 from django.contrib.auth.views import LoginView, LogoutView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -26,11 +24,18 @@ class Analytics(TemplateView):
     template_name = "calorietracker/analytics.html"
 
 
-class LogData(CreateView):
+class LogData(LoginRequiredMixin, CreateView):
     model = Log
     form_class = LogForm
     template_name = "calorietracker/logdata.html"
     success_url = reverse_lazy("analytics")
+
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class Register(CreateView):
