497bd9bcdf0c17ad68f6c78ed66b1cf26888220e
Balu <Balu+gitt@bluebeamproject.com>
2020-09-26 01:06:48 +0100

    Model SafeDelete Package installation

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "calorietracker",
+
+    # third party packages/apps
+    'safedelete',
 ]
 
 MIDDLEWARE = [
