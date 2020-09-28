57a03556808386dcc9fad860db78f8f7b6b10915
Balu <Balu+gitt@bluebeamproject.com>
2020-09-28 04:00:31 +0100

    Admin site name

--- mysite/calorietracker/admin.py
+++ mysite/calorietracker/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Setting, Log
 
+admin.site.site_header = 'CalorieTracker'
+admin.site.site_title = 'CalorieTracker'
+
 admin.site.register(Setting)
 admin.site.register(Log)
