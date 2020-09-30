4257e26cd0a31a134b6432ae44ae7c829312d63c
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-09-30 09:55:15 -0400

    Combine charts and analytics into just charts. Some clean up

--- mysite/calorietracker/urls.py
+++ mysite/calorietracker/urls.py
@@ -5,15 +5,11 @@ urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
     path("profile/", views.Profile.as_view(), name="profile"),
     path("logdata/", views.LogData.as_view(), name="logdata"),
-    path("charts/", views.Charts.as_view(), name="charts"),
     path("analytics/", views.Analytics.as_view(), name="analytics"),
-
     # AUTH
     path("register/", views.Register.as_view(), name="register"),
     path("login/", views.Login.as_view(), name="login"),
     path("logout/", views.Logout.as_view(), name="logout"),
     path("change-password/", views.PasswordChange.as_view(), name="change-password"),
-
     path("settings/", views.Settings.as_view(), name="settings"),
-
 ]
