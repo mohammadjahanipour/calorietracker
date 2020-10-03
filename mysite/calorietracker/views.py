5ed0313f76f182e70291aaf8faf6fafd430487b7
Balu <Balu+gitt@bluebeamproject.com>
2020-10-03 18:34:57 +0100

    flash messages

--- mysite/calorietracker/views.py
+++ mysite/calorietracker/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.db import transaction
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, CreateView, FormView, UpdateView
