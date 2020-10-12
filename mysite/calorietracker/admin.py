15f01669b2ddaa5c2cbd9b9b216adf865a637b79
Balu <Balu+gitt@bluebeamproject.com>
2020-10-12 19:19:27 +0100

    Feedback Admin View

--- mysite/calorietracker/admin.py
+++ mysite/calorietracker/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Setting, Log
+from .models import Setting, Log, Feedback
 
 admin.site.site_header = 'CalorieTracker'
 admin.site.site_title = 'CalorieTracker'
 
 admin.site.register(Setting)
 admin.site.register(Log)
+admin.site.register(Feedback)
