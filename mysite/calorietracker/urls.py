20efe0be2eabd0ac5667aa0a1cb9815af749c081
Balu <Balu+gitt@bluebeamproject.com>
2020-10-14 08:55:26 +0100

    url routing for importing mfp credentials

--- mysite/calorietracker/urls.py
+++ mysite/calorietracker/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path("profile/", views.Profile.as_view(), name="profile"),
     path("logdata/", views.LogData.as_view(), name="logdata"),
     path("logs/", views.ViewLogs.as_view(), name="logs"),
-    path("import/", views.Import.as_view(), name="import"),
-    path("logdata/<pk>/edit", views.UpdateLogData.as_view(), name="UpdateLogData"),
+    path("logdata/<pk>/edit", views.UpdateLogData.as_view(), name="UpdateLogData"),  # TODO: switch to update naming instead of edit for consistancy
     path("analytics/", views.Analytics.as_view(), name="analytics"),
     path("chartJSON", line_chart_json, name="line_chart_json"),
     # AUTH
@@ -19,4 +18,9 @@ urlpatterns = [
     path("change-password/", views.PasswordChange.as_view(), name="change-password"),
     path("settings/", views.Settings.as_view(), name="settings"),
     path("feedback/", views.Feedback.as_view(), name="feedback"),
+
+    path("import/", views.Import.as_view(), name="import"),
+    path("import/credentials/mfp/", views.ImportMFPCredentials.as_view(), name="import-credentials-mfp"),
+    path("import/credentials/mfp/create/", views.ImportMFPCredentialsCreate.as_view(), name="import-credentials-mfp-create"),
+    path("import/credentials/mfp/update/", views.ImportMFPCredentialsUpdate.as_view(), name="import-credentials-mfp-update"),
 ]
