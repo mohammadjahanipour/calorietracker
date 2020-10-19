935eef26d210f29089ca136bc2410a2e98ce8e6d
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-10-18 21:15:17 -0400

    Revert "catch all logging"
    This reverts commit a515d28ffce3261e46bb972b600571000d224bae.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,7 +50,6 @@ LOGGING = {
         },
     },
     "loggers": {
-        "": {"handlers": ["file", "mail_admins"], "level": "DEBUG", "propagate": True},
         "django": {
             "handlers": ["file", "mail_admins"],
             "level": "DEBUG",
