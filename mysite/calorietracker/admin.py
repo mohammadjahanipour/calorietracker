c70c91ace50e66a5b5823f38b3f44e4d7632e4ef
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-10-21 18:15:36 -0400

    get all calorietracker models in admin panel

--- mysite/calorietracker/admin.py
+++ mysite/calorietracker/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-
-from .models import Setting, Log, Feedback, Streak, Subscription, MFPCredentials, Wallet
+from django.apps import apps
 
 admin.site.site_header = "CalorieTracker"
 admin.site.site_title = "CalorieTracker"
 
-admin.site.register(Setting)
-admin.site.register(Log)
-admin.site.register(Feedback)
-admin.site.register(Streak)
-admin.site.register(Subscription)
-admin.site.register(MFPCredentials)
-admin.site.register(Wallet)
+myapp = apps.get_app_config("calorietracker")
+for model in myapp.get_models():
+    admin.site.register(model)
