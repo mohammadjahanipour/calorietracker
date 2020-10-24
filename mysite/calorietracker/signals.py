c0c6244f53ae7d88791c8a54ffc905f813f65e11
Balu <Balu+gitt@bluebeamproject.com>
2020-10-24 13:56:54 +0100

    Credit referrers with coins signal

--- mysite/calorietracker/signals.py
+++ mysite/calorietracker/signals.py
@@ -4,7 +4,26 @@ from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Setting, Streak
+from .models import Setting, Streak, Wallet
+from pinax.referrals.models import Referral, ReferralResponse
+from pinax.referrals.signals import user_linked_to_response
+
+
+@receiver(user_linked_to_response)
+def handle_user_linked_to_response_credit_referrer(sender, response, **kwargs):
+    # credits the referer with 10 coins
+
+    if response.action == "RESPONDED":
+
+        # only credit one Response otherwise social logins via a ref link will be credited repeatedly
+        count = ReferralResponse.objects.filter(user=response.user).count()
+        if count >= 2:
+            return
+
+        referrer = response.referral.user
+        Wallet.objects.update_or_create(user=referrer)  # create wallet if not exists
+        referrer.wallet.coins += 10
+        referrer.wallet.save()
 
 
 @receiver(post_save, sender=get_user_model())
