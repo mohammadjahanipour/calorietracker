276c792217aa13f08bc3620c51a11327866024fc
kb1900 <krishna.bikkasani@icahn.mssm.edu>
2020-11-03 13:51:53 -0500

    mfp_autosync model field, form field, initial sync

--- mysite/calorietracker/management/commands/mfp_sync.py
+++ mysite/calorietracker/management/commands/mfp_sync.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from ... import views
 
 
 class Command(BaseCommand):
-    help = 'Syncs MFP Data to our DB'
+    help = "Syncs MFP Data to our DB"
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Synced"))
