f011c5c96fe9c42fee8902d6eebf4c18c3590871
Balu <Balu+gitt@bluebeamproject.com>
2020-11-23 22:03:18 +0000

    Register User model for actsream

--- mysite/calorietracker/apps.py
+++ mysite/calorietracker/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+
+
 
 
 class CalorietrackerConfig(AppConfig):
@@ -7,6 +10,17 @@ class CalorietrackerConfig(AppConfig):
     def ready(self):
         import calorietracker.signals  # signal dependendcy
 
+
+
         # Actsream
-        # from actstream import registry
-        # registry.register(self.get_model('SomeModel'))
+        from actstream import registry
+
+        # registry.register(self.get_model('User'))
+
+        # User Model registration
+        # assumes default django user model
+        # needs adjustment if custom user model is used
+        # also not 100% sure if this is the correct way to handle this but no docs on it afaik
+
+        from django.contrib.auth.models import User
+        registry.register(User)
